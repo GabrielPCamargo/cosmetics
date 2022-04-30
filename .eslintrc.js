7c8f48d700546fb665b86bfe2eb6a0e088b07918
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2022-04-30 11:11:56 -0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'default-param-last': 0,
+  },
+};
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Page404() {
+  return (
+    <Container>
+      <h1>Está página não exite</h1>
+    </Container>
+  );
+}
